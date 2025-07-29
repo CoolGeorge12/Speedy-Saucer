22e552cf707c7c814228174d00c9f777c5044b91
CoolGeorge <zack87608@gmail.com>
2025-07-29 12:12:31 -0600

    movement acquired

--- speedy-saucer/player.gd
+++ speedy-saucer/player.gd
@@ -1,9 +1,12 @@
 extends RigidBody2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	test()
-
-func test ():
-	print("This is the test function from the player script")
-	
+func _physics_process(delta: float) -> void:
+	var force = 1000
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2(force, 0))
+	if Input.is_action_pressed("move_left"):
+		apply_force(Vector2(-force, 0))
+	if Input.is_action_pressed("move_down"):
+		apply_force(Vector2(0, force))
+	if Input.is_action_pressed("move_up"):
+		apply_force(Vector2(0, -force))
