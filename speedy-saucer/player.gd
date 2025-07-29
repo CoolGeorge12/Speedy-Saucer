91e13fe7c1ab742c109eb51c05839f8925b542ea
CoolGeorge <zack87608@gmail.com>
2025-07-29 16:58:42 -0600

    must fix problems

--- speedy-saucer/player.gd
+++ speedy-saucer/player.gd
@@ -1,7 +1,12 @@
 extends RigidBody2D
 
+var force = 1500
+
+func _ready() -> void:
+	var addition_result = add(10, 5)
+	print(addition_result)
+
 func _physics_process(delta: float) -> void:
-	var force = 1000
 	if Input.is_action_pressed("move_right"):
 		apply_force(Vector2(force, 0))
 	if Input.is_action_pressed("move_left"):
@@ -10,3 +15,10 @@ func _physics_process(delta: float) -> void:
 		apply_force(Vector2(0, force))
 	if Input.is_action_pressed("move_up"):
 		apply_force(Vector2(0, -force))
+
+func test():
+	var t = 10
+	return (t + 10)
+
+func add(num1, num2):
+	return num1 + num2
